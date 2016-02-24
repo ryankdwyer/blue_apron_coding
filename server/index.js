cbf5290da85c89132943c667cf2f4829cb00d5d5
Ryan Dwyer <ryan@Ryans-MacBook-Pro.local>
2016-02-24 18:35:56 -0500

    fixing port

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+let PORT = process.env.PORT || 1337;
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res, next) {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.listen(1337, function () {
+app.listen(PORT, function () {
   console.log('Listening on port: 1337');
 });
